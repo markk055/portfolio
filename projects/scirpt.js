411793a4d7923166ce61a02d69f1ea39f4b6c1df
markk055 <143321116+markk055@users.noreply.github.com>
2023-12-30 13:04:08 +0700

    new

--- projects/scirpt.js
+++ projects/scirpt.js
@@ -1,19 +1,17 @@
-const slides = document.querySelectorAll('.slide');
-    let currentSlide = 0;
-
-    function showSlide(index) {
-      slides.forEach(slide => slide.style.display = 'none');
-      slides[index].style.display = 'block';
+$(document).ready(function(){
+  $('.entry').hover(function(){
+    var li = $('li', this);
+    
+    for(var i = 0; i < $(li).length; i++){
+      $($(li)[i]).stop().animate({
+      'height':'18px'
+    }, 300 + (i * 150));
     }
-
-    function nextSlide() {
-      currentSlide++;
-      if (currentSlide >= slides.length) {
-        currentSlide = 0;
-      }
-      showSlide(currentSlide);
-    }
-
-    setInterval(nextSlide, 2000); // Change slide every 3 seconds
-
-    showSlide(currentSlide);
+  }, function(){
+    $('li', this).stop().animate({
+      'height':'5px'
+    }, 250);
+  });
+   
+   
+ });
